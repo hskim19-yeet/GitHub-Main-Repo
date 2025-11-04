dd189559abe4681dea21cc5fc2cb9cf06b852a37
hskim19-yeet <hskim19@asu.edu>
2025-11-04 15:40:13 -0700

    migrating from Germany to US Ohio.
    Recreating the EC2 and RDS from Noah's to my(Hyun's) AWS.

--- working-directory/app.py
+++ working-directory/app.py
@@ -14,8 +14,8 @@ app = Flask(__name__)
 
 AZ = ZoneInfo("America/Phoenix")
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:IFT401@localhost/stockcraft_db'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:password2025@database-1.cf4ak2q88oos.eu-central-1.rds.amazonaws.com/stockcraft_db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:IFT401@localhost/stockcraft_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:password@ift401capstonedb.cr2yo46oe8hh.us-east-2.rds.amazonaws.com/stockcraft_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'your-secret-key'
 
